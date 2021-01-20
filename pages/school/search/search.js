532e6857b52f6727b03dd984f925abd0914ea91f
hujian <>
2021-01-20 18:30:14 +0800

    同步代码

--- pages/school/search/search.js
+++ pages/school/search/search.js
@@ -9,31 +9,42 @@ Page({
     currentIndex: 0,
     tabs: [{
         id: 1,
-        name: '推荐'
+        name: '推荐',
+        pageIndex: 0,
+        type: 100
       },
       {
         id: 2,
-        name: '热门'
+        name: '热门',
+        pageIndex: 0,
+        type: 101
       },
       {
         id: 3,
-        name: '幼儿园'
+        name: '幼儿园',
+        pageIndex: 0,
+        type: 0
       },
       {
         id: 4,
-        name: '小学'
+        name: '小学',
+        pageIndex: 0,
+        type: 1
       },
       {
         id: 5,
-        name: '中学'
+        name: '中学',
+        pageIndex: 0,
+        type: 2
       },
       {
         id: 6,
-        name: '培训机构'
+        name: '培训机构',
+        pageIndex: 0,
+        type: 3
       }
     ],
-    recommonds: [
-      {
+    recommonds: [{
         school: {
           id: 11,
           name: '金苹果中海国际社区幼儿园',
@@ -46,7 +57,7 @@ Page({
         area: '成都高新区',
         addr: '成都高新区蓝岸17号',
         distanceStr: '10km',
-        tagSrc: '/images/kindergarten-tag.png'      
+        tagSrc: '/images/kindergarten-tag.png'
       },
       {
         school: {
@@ -77,7 +88,7 @@ Page({
         addr: '成都高新区蓝岸17号',
         distanceStr: '',
         tagSrc: '/images/middle-school-tag.png'
-      }      
+      }
     ], // 推荐
     hots: [], // 热门
     kindergartens: [], // 幼儿园
@@ -149,5 +160,107 @@ Page({
     this.setData({
       currentIndex: event.currentTarget.dataset.index
     })
+  },
+
+  switchPageData(index) {
+    let tabData = this.data.tabs[index]
+    var pageData = null
+    switch (index) {
+      case 0:
+        pageData = this.data.recommonds
+        break
+      case 1:
+        pageData = this.data.hots
+        break
+      case 2:
+        pageData = this.data.kindergartens
+        break
+      case 3:
+        pageData = this.data.primarys
+        break
+      case 4:
+        pageData = this.data.middles
+        break
+      case 5:
+        pageData = this.data.trainings
+        break
+    }
+
+    if (pageData.length == 0) {
+
+    }
+  },
+
+  refreshLoadPage(tabData, pageData) {
+
+    if (tabData.pageIndex == 0) {
+      pageData = []
+    }
+
+    wx.request({
+      url: app.globalData.baseUrl + '/school/getSchoolList',
+      header: {
+        'token': app.globalData.token,
+        'content-type': 'application/json'
+      },
+      data: {
+        type: tabData.type,
+        pageIndex: tabData.pageIndex,
+        pageSize: 20,
+        lng: 0,
+        lat: 0
+      },
+      success(res) {
+        if (res.data.code != 0) {
+          console.error('refreshLoadPage success code != 0, msg ' + res.data.msg)
+          wx.showToast({
+            title: '获取数据失败 ' + res.data.msg,
+            icon: 'none'
+          })
+        } else {
+
+          var schoolExts = res.data.data
+          if (schoolExts == null) {
+            return
+          }
+
+          for (var i = 0; i < schoolExts.length; i++) {
+            var item = schoolExts[i]
+            item.nature = item.school.nature == 0 ? '公办' : '私办'
+            item.area = item.school.city_name + item.school.area_name
+            item.addr = item.school.province_name +
+              item.school.city_name +
+              item.school.area_name +
+              item.street_name +
+              item.detail_addr
+            
+            if (item.distance > 0) {
+              var km = item.distance / 1000
+              if (km < 1) {
+                
+              }
+            }  
+
+          }
+
+          that.setData({
+
+          })
+        }
+      },
+      fail(res) {
+        console.error('refreshLoadPage fail res ' + res.errMsg)
+        wx.showToast({
+          title: '获取数据失败 ' + res.errMsg,
+          icon: 'none'
+        })
+      },
+      complete(res) {
+        wx.hideLoading()
+      }
+    })
+
   }
+
+},
 })
