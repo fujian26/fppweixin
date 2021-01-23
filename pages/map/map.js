0a01f641a50c7456ad65d80aee9c8050775c8766
hujian <>
2021-01-23 18:13:55 +0800

    同步代码

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,5 +1,21 @@
 // pages/map/map.js
+let app = getApp()
 Component({
+
+  attached() {
+    let that = this
+    wx.createSelectorQuery()
+      .in(that)
+      .select('#basebar')
+      .boundingClientRect()
+      .exec(function (res) {
+        console.log('createSelectorQuery res ' + res[0].height)
+        that.setData({
+          mapHeight: app.globalData.screenHeight - res[0].height
+        })
+      })
+  },
+
   options: {
     addGlobalClass: true,
   },
@@ -14,7 +30,9 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    mapHeight: 0,
+    lng: app.globalData.lng,
+    lat: app.globalData.lat
   },
 
   /**
