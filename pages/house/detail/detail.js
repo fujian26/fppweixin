4c8bd0053809fb68b912c6b275227de2a9557fc1
fujian26 <fujian26@sina.com>
2021-03-03 22:36:50 +0800

    房源页面-关注逻辑

--- pages/house/detail/detail.js
+++ pages/house/detail/detail.js
@@ -15,7 +15,9 @@ Page({
       '同小区房源',
       '周边房源',
       '划片小区'
-    ]
+    ],
+    commentNum: 0,
+    attentioned: false
   },
 
   /**
@@ -47,6 +49,9 @@ Page({
         console.log('house name ' + house.name)
 
         var showAddTime = house.add_time.split(' ')[0]
+
+        that.queryAttention(house.id)
+        that.queryComment(house.id)
 
         that.setData({
           house: house,
@@ -163,6 +168,162 @@ Page({
       fail(res) {
         console.error(tag + ' setStorage community fail ' + res.errMsg)
       }
-    })  
+    })
+  },
+
+  tapComment(event) {
+    console.log('tapComment')
+  },
+
+  tapAttention(event) {
+
+    console.log('tapAttention')
+
+    let that = this
+    var attentioned = this.data.attentioned
+    var house = this.data.house
+
+    wx.showLoading({
+      title: '',
+    })
+
+    wx.request({
+      url: app.globalData.baseUrl + '/house/attentionHouse',
+      method: 'POST',
+      header: {
+        'token': app.globalData.token,
+        'content-type': 'application/json'
+      },
+      data: {
+        "houseId": house.id,
+        "attention": !attentioned
+      },
+      success(res) {
+        console.log(tag + ' tapAttention success')
+        if (res.data.code != 0) {
+          console.error(tag + ' tapAttention success code != 0, msg ' + res.data.msg)
+          wx.showToast({
+            title: '操作失败 ' + res.data.msg,
+            icon: 'none'
+          })
+        } else {
+
+          that.setData({
+            attentioned: res.data.data.attentioned
+          })
+        }
+      },
+      fail(res) {
+        console.error(tag + ' tapAttention fail res ' + res.errMsg)
+        wx.showToast({
+          title: '操作失败 ' + res.errMsg,
+          icon: 'none'
+        })
+      },
+      complete(res) {
+        wx.hideLoading({
+          success: (res) => {},
+        })
+      }
+    })
+  },
+
+  tapConsulte(event) {
+    console.log('tapConsulte')
+  },
+
+  tapCall(event) {
+    console.log('tapCall')
+    let house = this.data.house
+    if (house.phone_num != null && house.phone_num.length > 0) {
+      wx.makePhoneCall({
+        phoneNumber: house.phone_num,
+      })
+    } else {
+      console.error(tag + ' tapCall house.phone_num == null || house.phone_num.length == 0')
+    }
+  },
+
+  queryAttention(houseId) {
+
+    let that = this
+
+    wx.request({
+      url: app.globalData.baseUrl + '/house/queryHouseAttention',
+      header: {
+        'token': app.globalData.token,
+        'content-type': 'application/json'
+      },
+      data: {
+        "houseId": houseId
+      },
+      success(res) {
+        console.log(tag + ' queryAttention success')
+        if (res.data.code != 0) {
+          console.error(tag + ' queryAttention success code != 0, msg ' + res.data.msg)
+          wx.showToast({
+            title: '数据错误 ' + res.data.msg,
+            icon: 'none'
+          })
+        } else {
+
+          that.setData({
+            attentioned: res.data.data.attentioned
+          })
+        }
+      },
+      fail(res) {
+        console.error(tag + ' queryAttention fail res ' + res.errMsg)
+        wx.showToast({
+          title: '数据错误 ' + res.errMsg,
+          icon: 'none'
+        })
+      },
+      complete(res) {}
+    })
+
+  },
+
+  queryComment(houseId) {
+
+    let that = this
+
+    wx.request({
+      url: app.globalData.baseUrl + '/comments/getList',
+      header: {
+        'token': app.globalData.token,
+        'content-type': 'application/json'
+      },
+      data: {
+        "id": houseId,
+        "type": 1,
+        "pageIndex": 0,
+        "pageSize": 20
+      },
+      success(res) {
+        console.log(tag + ' queryComment success')
+        if (res.data.code != 0) {
+          console.error(tag + ' queryComment success code != 0, msg ' + res.data.msg)
+          wx.showToast({
+            title: '数据错误 ' + res.data.msg,
+            icon: 'none'
+          })
+        } else {
+
+          that.setData({
+            commentNum: res.header.total
+          })
+        }
+      },
+      fail(res) {
+        console.error(tag + ' queryComment fail res ' + res.errMsg)
+        wx.showToast({
+          title: '数据错误 ' + res.errMsg,
+          icon: 'none'
+        })
+      },
+      complete(res) {}
+    })
+
   }
 })
